d6fec3bd5087d878b74ddc77e8e66aa152821469
danioscx <admoscop@gmail.com>
2022-03-09 09:33:26 +0700

    update dependency

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from flask import Flask
 from src.utils import db, bcrypt
 from src.utils.jwt import jwt
 from src.routers.users import users
+from src.routers.address import address
 
 
 def create_test_app():
     app = Flask(__name__)
 
     app.register_blueprint(users)
+    app.register_blueprint(address)
 
     app.config['JWT_SECRET_KEY'] = 'supper-secret'
     app.config["JWT_ERROR_MESSAGE_KEY"] = "message"
@@ -25,13 +27,13 @@ def create_debug_app():
     app = Flask(__name__)
 
     app.register_blueprint(users)
+    app.register_blueprint(address)
 
     app.config['JWT_SECRET_KEY'] = 'debug-secret'
     app.config["JWT_ERROR_MESSAGE_KEY"] = "message"
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}/db.sqlite'.format(
         os.getcwd())
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-
     jwt.init_app(app)
     db.init_app(app)
     bcrypt.init_app(app)
@@ -42,13 +44,13 @@ def create_release_app():
     app = Flask(__name__)
 
     app.register_blueprint(users)
+    app.register_blueprint(address)
 
     app.config['JWT_SECRET_KEY'] = os.urandom(20)
     app.config["JWT_ERROR_MESSAGE_KEY"] = "message"
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}/db.sqlite'.format(
         os.getcwd())
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-
     jwt.init_app(app)
     db.init_app(app)
     bcrypt.init_app(app)
@@ -59,4 +61,3 @@ if __name__ == "__main__":
     apps = create_test_app()
     apps.app_context().push()
     apps.run()
-    pass
--- src/routers/users.py
+++ src/routers/users.py
@@ -1,18 +1,37 @@
+import json
 from flask import Blueprint, jsonify, request
 
-from src.models.users import Users
-from src.utils import bcrypt, db
+from src.models.users import ShippingAddress, Users
+from src.utils import bcrypt, db, default_encoder
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 
 users = Blueprint(
-    "users", 
+    "users",
     __name__,
     url_prefix="/api/v1/user"
 )
 
-@users.route("/")
-def index():
-    return ""
+
+@users.route("/me", methods=['GET', 'POST'])
+@jwt_required()
+def me():
+    user_id = get_jwt_identity()
+    shipping_address = ShippingAddress.query.filter_by(
+        user_id=user_id).one_or_none()
+    if shipping_address is None:
+        user = Users.query.filter_by(id=user_id).one_or_none()
+    else:
+        user = Users.query.join(
+            ShippingAddress, Users.id == ShippingAddress.user_id
+        ).filter(
+            (ShippingAddress.active == True) |
+            (Users.id == user_id)
+        ).one_or_none()
+    if user is not None:
+        return json.dumps(user, default=default_encoder), 200
+    else:
+        return jsonify({'message': 'UnAuthorization'}), 403
 
 
 @users.route("/signup", methods=['GET', 'POST'])
@@ -23,18 +42,22 @@ def sign_up():
         password = request.json.get("password", None)
         try:
             if username is None:
-                db.session.add(Users(email=email, password=bcrypt.generate_password_hash(password)))
+                db.session.add(
+                    Users(email=email, password=bcrypt.generate_password_hash(password)))
             elif email is None or password is None:
                 return jsonify({
                     'message': 'username or password cannot be null'
                 })
             else:
-                db.session.add(Users(email=email, username=username, password=bcrypt.generate_password_hash(password)))
-            if db.session.commit() is None:
-                return jsonify({
-                    'message': 'success created user'
-                }), 200
-        except Exception as e:             
+                db.session.add(Users(email=email, username=username,
+                               password=bcrypt.generate_password_hash(password)))
+
+            db.session.commit()
+            return jsonify({
+                'message': 'success created user'
+            }), 200
+        except Exception as e:
+            db.session.rollback()
             return jsonify({
                 'message': 'failed created user email is already usage {}'.format(e)
             }), 409
@@ -44,17 +67,17 @@ def sign_up():
         }), 503
 
 
-
 @users.route("/signin", methods=['GET', 'POST'])
 def sign_in():
     if request.method == 'POST':
-        json = request.get_json()
-        email = json['email']
-        password = json['password']
+        email = request.json.get("email", None)
+        password = request.json.get("password", None)
         user = Users.query.filter_by(email=email).one_or_none()
         if user is not None and bcrypt.check_password_hash(user.password, password):
+            access_token = create_access_token(identity=user)
             return jsonify({
-                'message': 'success sign in'
+                'message': 'success sign in',
+                'token': access_token
             }), 200
         else:
             return jsonify({
